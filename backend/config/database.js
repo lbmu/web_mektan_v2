48c2c538d88fa97485b29b59d173808176ee0858
Khalid Nurahman <mynkhalid@gmail.com>
2026-06-11 10:41:45 +0700

    add

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,11 +1,12 @@
 const { Pool } = require('pg');
 
 const db = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
+    // Menggunakan Connection String tunggal yang diberikan Neon
+    connectionString: process.env.DATABASE_URL,
+    // Wajib diaktifkan agar Neon.tech mengizinkan koneksi dari luar
+    ssl: {
+        rejectUnauthorized: false 
+    }
 });
 
 db.connect((err, client, release) => {
